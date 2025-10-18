153014a15104df0dfa0918e17d55400a020c74a5
fangguisheng520 <fangguisheng@outlook.com>
2025-10-18 21:01:03 +0800

    新增原始数据数量

--- models/categoryModel.go
+++ models/categoryModel.go
@@ -11,6 +11,7 @@ type IptvCategory struct {
 	AutoCategory int64  `gorm:"column:autocategory" json:"autocategory"`
 	Repeat       int64  `gorm:"column:repeat" json:"repeat"`
 	Sort         int64  `gorm:"column:sort" json:"sort"`
+	Rawcount     int64  `gorm:"column:rawcount" json:"rawcount"`
 }
 
 func (IptvCategory) TableName() string {
