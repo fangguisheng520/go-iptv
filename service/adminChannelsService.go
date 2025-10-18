719cefdb1d2f637eb9ca0896c05a223ae355f6c3
fangguisheng520 <fangguisheng@outlook.com>
2025-10-18 22:14:12 +0800

    新增原始数据数量

--- service/adminChannelsService.go
+++ service/adminChannelsService.go
@@ -735,7 +735,7 @@ func AddChannelList(cname, srclist string, doRepeat bool) (int, error) {
 		}
 	}
 	log.Println("原始有效频道数量:", rawCount) // 新增日志输出
-	dao.DB.Model(&models.IptvCategory{}).Where("name = ?", cname).Update("quantity", rawCount)
+	dao.DB.Model(&models.IptvCategory{}).Where("name = ?", cname).Update("rawcount", rawCount)
 
 	// 批量删除数据库中当前分类但新列表中没有的 URL
 	for _, ch := range oldChannels {
