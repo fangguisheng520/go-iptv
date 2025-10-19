02341eee976418ff249a3b76535c79060b5d42dd
fangguisheng520 <fangguisheng@outlook.com>
2025-10-19 11:22:36 +0800

    修改随机更新EPG 1:00:00-5:59:59 范围

--- crontab/epgList.go
+++ crontab/epgList.go
@@ -1,8 +1,10 @@
 package crontab
 
 import (
+	"fmt"
 	"go-iptv/until"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/robfig/cron/v3"
@@ -11,16 +13,25 @@ import (
 func EpgCron() {
 	c := cron.New(cron.WithSeconds()) // 支持秒级别 cron 表达式
 
-	// 每天凌晨1点执行
+	// 生成1:00:00到5:59:59之间的随机时间
+	rand.Seed(time.Now().UnixNano())
+	hour := rand.Intn(5) + 1 // 1-5点
+	minute := rand.Intn(60)  // 0-59分
+	second := rand.Intn(60)  // 0-59秒
+
+	// 构建cron表达式（秒 分 时 * * *）
+	cronExpr := fmt.Sprintf("%d %d %d * * *", second, minute, hour)
+	log.Printf("设置随机EPG自动更新时间为: %02d:%02d:%02d", hour, minute, second)
+
 	// cron 表达式格式: 秒 分 时 日 月 星期
 	// 下面表示每天 01:00:00
-	c.AddFunc("0 0 1 * * *", func() {
-		log.Println("更新EPG列表任务开始执行:", time.Now().Format("2006-01-02 15:04:05"))
+	c.AddFunc(cronExpr, func() {
+		log.Println("自动更新EPG列表任务开始执行:", time.Now().Format("2006-01-02 15:04:05"))
 		// 在这里写你的任务逻辑
 		if until.UpdataEpgList() {
-			log.Println("更新EPG列表任务执行成功:", time.Now().Format("2006-01-02 15:04:05"))
+			log.Println("自动更新EPG列表任务执行成功:", time.Now().Format("2006-01-02 15:04:05"))
 		} else {
-			log.Println("更新EPG列表任务执行失败:", time.Now().Format("2006-01-02 15:04:05"))
+			log.Println("自动更新EPG列表任务执行失败:", time.Now().Format("2006-01-02 15:04:05"))
 		}
 	})
 	c.Start()
